20e61000c3e2046dcbdb168f1aeabf11d26c197f
Hanlaoss <asd159632874@gmail.com>
2024-01-23 09:22:34 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class ApiController : Controller
+	{
+		private readonly MyDBContext _dbContext;
+
+        public ApiController(MyDBContext dbContext)
+        {
+			_dbContext = dbContext;
+		}
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+		
+		public IActionResult Cities()
+		{
+			var cities = _dbContext.Addresses.Select(a=>a.City).Distinct();
+			return Json(cities);
+		}
+	}
+}
